7d1dba577d782dec06d9bf1e37cd909b43b7c8f3
Mark Oborne <markoborne1@gmail.com>
2026-04-11 20:26:35 +0900

    remove custom nvim package

--- update-custom-srcpkgs.sh
+++ update-custom-srcpkgs.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 pkgs=("$@")
-(( ${#pkgs[@]} == 0 )) && pkgs=(codex dmenu dwm fp lualib luarocks lus nvim prefix sent zig zls zua)
+(( ${#pkgs[@]} == 0 )) && pkgs=(codex dmenu dwm fp lualib luarocks lus prefix sent zig zls zua)
 
 latest_stable_tag() {
 	local repo="$1" prefix="$2"
@@ -52,29 +52,7 @@ for pkg in "${pkgs[@]}"; do
 		echo "$pkg: $version -> $newver"
 		version="$newver"
 	fi
-
-	if [[ "$pkg" == "nvim" ]]; then
-		deps_txt="$(curl -fsSL https://raw.githubusercontent.com/neovim/neovim/nightly/cmake.deps/deps.txt)"
-		luajit_url=$(echo "$deps_txt" | awk '$1=="LUAJIT_URL"{print $2; exit}')
-		luv_url=$(echo "$deps_txt" | awk '$1=="LUV_URL"{print $2; exit}')
-		lua_compat53_url=$(echo "$deps_txt" | awk '$1=="LUA_COMPAT53_URL"{print $2; exit}')
-		lpeg_url=$(echo "$deps_txt" | awk '$1=="LPEG_URL"{print $2; exit}')
-
-		luajit_ver=$(basename "$luajit_url" .tar.gz)
-		luv_ver=$(basename "$luv_url" .tar.gz)
-		lua_compat53_ver=$(basename "$lua_compat53_url" .tar.gz)
-		lua_compat53_ver="${lua_compat53_ver#v}"
-		lpeg_ver=$(basename "$lpeg_url" .tar.gz)
-		lpeg_ver="${lpeg_ver#lpeg-}"
-		lpeg_deps_commit=$(echo "$lpeg_url" | sed -E 's#^https://github.com/neovim/deps/raw/([^/]+)/opt/lpeg-.*#\1#')
-
-		sed -i -E "s|^_luajit_version=.*|_luajit_version=$luajit_ver|" "$template"
-		sed -i -E "s|^_luv_version=.*|_luv_version=$luv_ver|" "$template"
-		sed -i -E "s|^_lua_compat53_version=.*|_lua_compat53_version=$lua_compat53_ver|" "$template"
-		sed -i -E "s|^_lpeg_version=.*|_lpeg_version=$lpeg_ver|" "$template"
-		sed -i -E "s|^_lpeg_deps_commit=.*|_lpeg_deps_commit=$lpeg_deps_commit|" "$template"
-		exit 0
-	fi
+fi
 
 	distfile=$(./xbps-src show "$pkg" | awk '/distfiles/ { print $2 }')
 	checksum=$(sed -n 's/^checksum=//p' "$template")
